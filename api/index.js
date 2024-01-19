5aa9a6a43660dfe65d5dd4932faeda586ec67233
Yan Moreno <morenofranco77@gmail.com>
2024-01-18 21:43:51 -0500

    Variables entorno

--- api/index.js
+++ api/index.js
@@ -1,12 +1,9 @@
-require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-const {PORT} = process.env;
-
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`%s listening at`, process.env.PORT); // eslint-disable-line no-console
+  server.listen(3001, () => {
+    console.log(`%s listening at 3001`); // eslint-disable-line no-console
   });
 });
