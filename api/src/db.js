8b805e04fe9d5e180cae50f2b99ea05fbb4320ce
Yan Moreno <morenofranco77@gmail.com>
2024-01-18 21:49:40 -0500

    Variables entorno 1.0

--- api/src/db.js
+++ api/src/db.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+const {
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_PORT
+} = process.env;
 
-const sequelize = new Sequelize(`postgres://postgres:admin@localhost:5432/countries`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
